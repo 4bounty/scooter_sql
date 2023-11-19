75c4410f5e9667002ed21f8e7966169a0eb6f7a4
4bounty <148626184+4bounty@users.noreply.github.com>
2023-11-20 00:33:51 +0300

    Update test_order.py

--- test_order.py
+++ test_order.py
@@ -1,22 +1,13 @@
 #Денис Ромашкин 10я когорта, Дипломный проект
-import pytest
-import requests
-import configuration
-import data
+import sender_stand_requests
 
-# Фикстура для создания заказа
-@pytest.fixture
-def created_order():
-    # Выполняем запрос на создание заказа
-    response = requests.post(configuration.BASE_URL + configuration.KITS_PATH, json=data.orders_body)
-    # Проверяем успешность запроса на создание заказа
-    assert response.status_code == 201
-    # Извлекаем номер заказа (track) из ответа JSON и возвращаем его
-    return response.json().get('track')
-
-def test_get_order_by_track(created_order):
-    # Выполняем запрос на получение заказа по треку заказа
-    url = f'{configuration.BASE_URL}{configuration.RECEIVE_ORDER}{created_order}'
-    response = requests.get(url)
-    # Проверяем, что код ответа равен 200
+# Запрос на создание нового заказа
+def test_get_order_by_track():
+    #Вызываю создание заказа
+    new_response = sender_stand_requests.create_order()
+    #Извлекаю трек из ответа созданного заказа
+    track = new_response.json()['track']
+    #ВВыполняем запрос на получение заказа по треку заказа
+    response = sender_stand_requests.get_orders_by_track(track)
+    #Проверем, что код ответа равен 200
     assert response.status_code == 200
